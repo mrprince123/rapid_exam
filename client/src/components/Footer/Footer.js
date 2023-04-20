6fc8c655147f5ed608c16a4034db147d4d1168e6
Paresh <pareshtripathy2003@gmail.com>
2023-04-20 23:45:59 +0530

    recent update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -12,24 +12,26 @@ const Footer = () => {
                     <img src={logo} alt="rapid_logo" />
                     <p> The Ultimate Guide to <br />Pass in the Exam  </p>
                 </div>
+                <div className='footer-childs '>
                 <div className="rapid__footer-links_div">
                     <h4>MENU</h4>
                     <p>About US</p>
                     <p>Courses</p>
                     <p>Contact</p>
                 </div>
-                <div className="rapid__footer-links_div">
+                <div className="rapid__footer-links_div vertical_line">
                     <h4>Company</h4>
                     <p>Terms & Conditions </p>
                     <p>Privacy Policy</p>
                     
                 </div>
-                <div className="rapid__footer-links_div">
+                <div className="rapid__footer-links_div  get_in_touch  ">
                     <h4>Get in touch</h4>
-                    
                     <p>pareshtripathy071@gmail.com</p>
                 </div>
             </div>
+            </div>
+            <hr/>
 
             <div className="rapid__footer-copyright">
                 <p>@paresh the </p>
